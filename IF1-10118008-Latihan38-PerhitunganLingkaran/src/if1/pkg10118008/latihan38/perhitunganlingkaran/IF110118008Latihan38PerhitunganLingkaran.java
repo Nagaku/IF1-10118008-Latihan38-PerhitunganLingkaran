d98a25a3f8d600b39bcd6fb25a797d73f055b8ad
Nagaku <45015676+Nagaku@users.noreply.github.com>
2019-10-17 19:19:29 +0700

    Add files via upload

---
+++ IF1-10118008-Latihan38-PerhitunganLingkaran/src/if1/pkg10118008/latihan38/perhitunganlingkaran/IF110118008Latihan38PerhitunganLingkaran.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118008.latihan38.perhitunganlingkaran;
+
+/**
+ *
+* @author
+ * NAMA			: Michael Nagaku Milenn Salim
+ * KELAS		: IF-1
+ * NIM			: 10118008
+ * Deskripsi Program	: menghitung luas dan keliling lingkaran
+ */
+public class IF110118008Latihan38PerhitunganLingkaran {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		Hitung hitung = new Hitung();
+		hitung.inputDiameter();
+		hitung.output();
+	}
+	
+}
